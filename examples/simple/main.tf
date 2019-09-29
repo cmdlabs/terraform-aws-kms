ac57aa3c2617c865ab69ceafb9c49d8e43bbc969
Alex Harvey <alexharv074@gmail.com>
2019-09-29 20:31:01 +1000

    Create role dependency in test
    Without this patch applied, terraform apply would fail with:
    
      Error: MalformedPolicyDocumentException: Policy contains a statement with one or more invalid principals.
            status code: 400, request id: 966acdeb-2ef8-4329-be20-f0d64293d181
    
    The issue being that the IAM role that this module depends upon was not
    present.

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,3 +1,28 @@
+locals {
+  client_name = "our_awesome_client"
+  role_name   = "${local.client_name}-role-console-breakglass"
+}
+
+resource "aws_iam_role" "our_awesome_client" {
+  name = local.role_name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 module "source" {
   source      = "../../"
   client_name = "our_awesome_client"
